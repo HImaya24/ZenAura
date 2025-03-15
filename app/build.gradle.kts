8a96c1fa4697e93506d31f2301cebc9f65d8352f
HImaya_24 <himayapalliyaguruge@gmail.com>
2025-03-15 19:22:34 +0530

    initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.zenaura_yourpersonaltherapist"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.zenaura_yourpersonaltherapist"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
---
+++ app/src/main/java/com/example/zenaura_yourpersonaltherapist/Onboarding1.kt
@@ -0,0 +1,26 @@
+package com.example.zenaura_yourpersonaltherapist
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Onboarding1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_onboarding1)
+
+        findViewById<Button>(R.id.nextButton).setOnClickListener {
+            startActivity(Intent(this, Onboarding2Activity::class.java))
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
